35e5ffcf47e81b387cdc05c5f7983249d2610b8f
Philip-Liu <60823834+philly-boop@users.noreply.github.com>
2023-04-03 10:57:02 -0400

    Update index.test.js

--- server/routes/__test__/index.test.js
+++ server/routes/__test__/index.test.js
@@ -3,14 +3,14 @@ const request = require('supertest');
 
 describe('register', () => {
     it('returns status code 200 if user is registered sucessfully', async () => {
-        const res = await request(app).post('/register').send({ firstName: 'John', lastName: 'Doe', email: 'john.doe@gmail.com', password: 'johndoe', passwordVerify: 'johndoe' });
+        const res = await request(app).post('/register').send({ firstName: 'John', lastName: 'Doe', email: 'john.doe@gmail.com', password: 'johndoe123', passwordVerify: 'johndoe' });
 
         expect(res.statusCode).toEqual(200);
     })
 
     describe('login', () => {
         it('returns status code 200 if user is logged in sucessfully', async () => {
-            const res = await request(app).post('/login').send({ email: 'john.doe@gmail.com', password: 'johndoe' });
+            const res = await request(app).post('/login').send({ email: 'john.doe@gmail.com', password: 'johndoe123' });
     
             expect(res.statusCode).toEqual(200);
         })
